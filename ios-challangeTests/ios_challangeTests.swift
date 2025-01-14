9299f9c7d74eb4dbc90b5ea48f833fa1204cc5da
Bruno Soares da Silva Vieira <brunovieiradev@gmail.com>
2025-01-14 14:41:45 -0300

    Initial Commit

---
+++ ios-challangeTests/ios_challangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_challangeTests.swift
+//  ios-challangeTests
+//
+//  Created by Bruno Soares on 14/01/25.
+//
+
+import Testing
+@testable import ios_challange
+
+struct ios_challangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
